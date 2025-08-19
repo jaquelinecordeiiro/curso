2d62a07f7f5759eaea91cf03b3d92d44c1190196
Jaqueline Cordeiro <jaquelinecordeiro01@gmail.com>
2025-08-18 22:22:40 -0300

    criação de todo o javascript do pomodoro

---
+++ pomodoro/index.js
@@ -0,0 +1,35 @@
+const INITIAL_TIME_IN_SECONDS = 25 * 60; // 1500
+let TIME_IN_SECONDS = INITIAL_TIME_IN_SECONDS;
+let TIMER = null;
+
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const resetButton = document.getElementById('reset');
+
+playButton.addEventListener('click', starCounter);
+pauseButton.addEventListener('click', pauseCounter);
+resetButton.addEventListener('click', resetCounter);
+
+updateDom();
+
+function starCounter() {    
+   TIMER = setInterval(() => {
+        TIME_IN_SECONDS--;
+        updateDom();
+    }, 1000);
+}
+function pauseCounter() {
+    clearInterval(TIMER);
+}
+function resetCounter() {
+    clearInterval(TIMER);
+    TIME_IN_SECONDS = INITIAL_TIME_IN_SECONDS;
+    updateDom();
+}
+function updateDom(){
+    const minutes = Math.floor(TIME_IN_SECONDS / 60);
+    document.getElementById('min').innerHTML = String(minutes).padStart(2, 0);
+
+    const seconds = TIME_IN_SECONDS % 60;
+    document.getElementById('sec').innerHTML = String(seconds).padStart(2, 0);
+}
