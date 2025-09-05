4485273afa91128a4b862817447166f64b23bb9b
Jaqueline Cordeiro <jaquelinecordeiro01@gmail.com>
2025-09-05 17:47:02 -0300

    adicionando responsividade e alinahndo as configurações do carrocel de imagens

--- imperio/index.js
+++ imperio/index.js
@@ -2,8 +2,8 @@ const previousEl = document.getElementById('previous');
 const nextEl = document.getElementById('next');
 const sliderEl = document.getElementById('slider');
 
-previousEl.addEventListener('click'), onPreviusClick;
-nextEl.addEventListener('click'), onNextClick;
+previousEl.addEventListener('click', onPreviusClick);
+nextEl.addEventListener('click', onNextClick);
 
 function onPreviusClick(){
     const sliderWidth = sliderEl.offsetWidth;
