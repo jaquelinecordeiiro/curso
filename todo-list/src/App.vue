db43683f5eec9bc1e9e5b287bcb3d1baa5d282c5
Jaqueline Cordeiro <jaquelinecordeiro01@gmail.com>
2025-10-09 17:48:04 -0300

    Projeto todolist

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,7 +1,9 @@
-<script setup></script>
+<script setup>
+    import TodoList from "./componentes/TodoList.vue";
+</script>
 
 <template>
-  
+  <TodoList />
 </template>
 
 <style scoped></style>
---
+++ todo-list/src/componentes/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <input type="text" @change="addToList" v-model="text">
+    <ul>
+        <li v-for="(item, index) in list " :key="index">
+            {{ item }}
+
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+      text:''
+    };
+  },
+  created(){
+    this.list = JSON.parse(localStorage.getItem('list'));
+    }
+  methods: {
+    addToList(){
+        this.list.unshift(this.text);
+        localStorage.setItem('list', JSON.stringify(this.list));
+        this.text = '';
+
+    }
+  }
+};
+
+</script>
+
+<style>
+
+</style>
