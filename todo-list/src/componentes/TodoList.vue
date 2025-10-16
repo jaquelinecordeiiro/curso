ac974637ddba5b9c9a1ee997477dbe21cb0d9328
Jaqueline Cordeiro <jaquelinecordeiro01@gmail.com>
2025-10-16 17:44:19 -0300

    criação e alterações do todo list

--- todo-list/src/componentes/TodoList.vue
+++ todo-list/src/componentes/TodoList.vue
@@ -1,11 +1,17 @@
 <template>
-    <input type="text" @change="addToList" v-model="text">
+  <div class="todo-list">
+    <h1>Todo-List</h1>
+    <input class="test-fiel" type="text" @change="addToList" v-model="text" />
     <ul>
-        <li v-for="(item, index) in list " :key="index">
-            {{ item }}
-
-        </li>
+      <li v-for="(item, index) in list" :key="index">
+        <span @click="toggleCheckBox(item)">
+          <input type="checkbox" :checked="item.done" name="checkbox" id="checkbox">
+          <span :class="{'done': item.done}">{{ item.label}}</span>
+        </span>
+        <span @click="deleteFromList(index)"> Delete </span>
+      </li>
     </ul>
+  </div>
 </template>
 
 <script>
@@ -13,24 +19,61 @@ export default {
   data() {
     return {
       list: [],
-      text:''
+      text: "",
     };
   },
-  created(){
-    this.list = JSON.parse(localStorage.getItem('list'));
-    }
+  created() {
+    this.list = JSON.parse(localStorage.getItem("list")) || [];
+  },
   methods: {
-    addToList(){
-        this.list.unshift(this.text);
-        localStorage.setItem('list', JSON.stringify(this.list));
-        this.text = '';
-
-    }
-  }
+    addToList() {
+      this.list.unshift({label: this.text, done: false});
+      this.updateLocalStorage();
+      this.text = "";
+    },
+    deleteFromList(index) {
+      this.list.splice(index, 1);
+      this.updateLocalStorage();
+    },
+    updateLocalStorage() {
+      localStorage.setItem("list", JSON.stringify(this.list));
+    },
+    toggleCheckBox(item) {
+      item.done = !item.done;
+      this.updateLocalStorage();
+    },
+  },
 };
-
 </script>
 
 <style>
-
+.todo-list {
+  width: 500px;
+  margin: auto;
+}
+h1{
+    text-align: center;
+    color: #42b983;
+}
+.test-fiel{
+    width: 100%;
+    height: 35px;
+    margin-bottom: 15px;
+    font-size: 20px;
+    padding: 5px;
+    box-sizing: border-box;
+}
+ul{
+    list-style: none;
+    padding: 0;
+}
+li{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;  
+}
+.done{
+    text-decoration: line-through;
+}
 </style>
