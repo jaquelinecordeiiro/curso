437d710442ba2dd4a41a0955e4fcdf0e5dc7db72
Jaqueline Cordeiro <jaquelinecordeiro01@gmail.com>
2025-11-17 16:22:31 -0300

    correção de erros

--- restaurante/restaurante/src/App.vue
+++ restaurante/restaurante/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
@@ -15,12 +17,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* seu CSS */
 </style>
+
