17f07831ffe9a7f94c9ecedeac45c5f4495649eb
Jaqueline Cordeiro <jaquelinecordeiro01@gmail.com>
2025-12-02 17:32:59 -0300

    começando o projeto adicionando backgroud e todas as variaveis

---
+++ restaurante-vue2/src/components/CategoryMenu.vue
@@ -0,0 +1,15 @@
+<template></template>
+    <div class="category-menu">
+
+    </div>
+
+<script>
+
+
+
+export default {
+  name: "CategoryMenu",
+};
+</script>
+
+<style></style>
--- restaurante-vue2/src/main.js
+++ restaurante-vue2/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../assets/css/globol.less'
 
 Vue.config.productionTip = false
 
