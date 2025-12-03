8e8314c474792a4a1b89892c2bcf93246fd8d3d3
Jaqueline Cordeiro <jaquelinecordeiro01@gmail.com>
2025-12-03 17:16:06 -0300

    criando o designer inicial da página

--- restaurante-vue2/src/components/CategoryMenu.vue
+++ restaurante-vue2/src/components/CategoryMenu.vue
@@ -1,14 +1,27 @@
-<template></template>
+<template>
     <div class="category-menu">
 
     </div>
+</template>
 
 <script>
 
+import Pizza from '../assets/icons/pizza.svg';
+import Combo from '../assets/icons/french-fries.svg';
+import Deserts from '../assets/icons/ice-cream.svg';
+import Drinks from '../assets/icons/smoothie.svg';
+import Burguer from '../assets/icons/food.svg';
 
 
 export default {
   name: "CategoryMenu",
+  components: {
+    Pizza,  
+    Combo,
+    Deserts,
+    Drinks,
+    Burguer,
+  },
 };
 </script>
 
