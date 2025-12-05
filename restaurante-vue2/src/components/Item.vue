ac2da4ea86346f2adba18cf4be4455e4c3e16733
Jaqueline Cordeiro <jaquelinecordeiro01@gmail.com>
2025-12-05 17:44:39 -0300

    adicionando cores

--- restaurante-vue2/src/components/Item.vue
+++ restaurante-vue2/src/components/Item.vue
@@ -1,24 +1,42 @@
 <template>
-    <div class="item">
-        <h3>{{ item.name }}</h3>
-        <p>{{ item.description }}</p>
-        <span>Price: ${{ item.price }}</span>
-    </div>
-
+  <div class="item">
+    <h2 class="item--name">{{ item.name }}</h2>
+    <p class="item--description">{{ item.description }}</p>
+    <span class="item--price">Price: ${{ item.price }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Item",
-    props: {
-        item: {
-           
-        }
-    }
-}
-
+  name: "Item",
+  props: {
+    item: {},
+  },
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+    .item {
+        width: 215.9486083984375;
+        height: 290;
+        border-radius: 8px;
+        background: white;
 
+
+        &--name {
+            font-weight: 600;
+            font-size: 18px;
+        }
+        &--description {
+            font-size: 12px;
+            color: @dark-grey;
+            font-weight: 300;
+        }
+        &--price{
+            color: @yellow;
+            font-size: 18px;
+            font-weight: 600;
+
+        }
+}
 </style>
