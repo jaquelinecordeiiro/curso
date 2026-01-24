e462cf3dcf817c7a83b10f4807a04e332b3ce8de
jaquelinecordeiiro <jaquelinecordeiro01@gmail.com>
2026-01-24 07:01:26 -0300

    criando tela de pagamento

--- restaurante-vue2/src/views/Payment.vue
+++ restaurante-vue2/src/views/Payment.vue
@@ -1,15 +1,41 @@
 <template>
-    <div class="payment-view">
-        <h1>Pagamento</h1>
+    <div class="payment">
+        <div class="payment-content">
+                <router-link to="/" class="payment--go-back" v-if="isSmallScreens()">← Voltar</router-link>
+            <Cart v-if="isDesktop()" />
+
+
+        </div>
     </div>
 </template>
 
 <script>
+import Mixins from "@/assets/mixins/mixins.js";
+import Cart from "@/components/Cart.vue";
+
 export default {
-    name: "PaymentView",
+    mixins: [Mixins],
+    components: {
+        Cart,
+    },
+
 };
 </script>
 
-<style lang="less" scoped>  
-    
+<style lang="less" scoped>
+.payment {
+    display: flex;
+    padding: 50px;
+
+    .payment-content {
+        flex-grow: 1;
+    }
+    .payment--go-back {
+    font-weight: 600;
+    font-size: 24px;
+    margin-bottom: 50px;
+    text-decoration: none;
+    color: @yellow;
+  }
+}
 </style>
