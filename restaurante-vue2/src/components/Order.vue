13e0ed9bf91a0790af1d3721aaf69ce275285f7b
jaquelinecordeiiro <jaquelinecordeiro01@gmail.com>
2026-02-09 21:17:02 -0300

    .

---
+++ restaurante-vue2/src/components/Order.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="order">
+        teste
+        
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "Order",
+};
+</script>
+
+<style lang="less" scoped>
+.order {
+    width: 720px;
+    background: white;
+    border-radius: 10px;
+    padding: 30px 50px;
+}
+</style>
--- restaurante-vue2/src/views/Payment.vue
+++ restaurante-vue2/src/views/Payment.vue
@@ -2,6 +2,7 @@
     <div class="payment">
         <div class="payment-content">
                 <router-link to="/" class="payment--go-back" v-if="isSmallScreens()">← Voltar</router-link>
+                <Order />
             <Cart v-if="isDesktop()" />
 
 
@@ -12,11 +13,13 @@
 <script>
 import Mixins from "@/assets/mixins/mixins.js";
 import Cart from "@/components/Cart.vue";
+import Order from "@/components/Order.vue";
 
 export default {
     mixins: [Mixins],
     components: {
         Cart,
+        Order,
     },
 
 };
@@ -24,18 +27,28 @@ export default {
 
 <style lang="less" scoped>
 .payment {
-    display: flex;
-    padding: 50px;
+  display: flex;
+  padding: 50px;
 
-    .payment-content {
-        flex-grow: 1;
-    }
-    .payment--go-back {
+  .payment-content {
+    flex-grow: 1;
+  }
+
+  .payment--go-back {
+    display: inline-block;
+    margin-bottom: 20px;
     font-weight: 600;
-    font-size: 24px;
-    margin-bottom: 50px;
+    font-size: 18px;
     text-decoration: none;
     color: @yellow;
+  }
+}
+
+/* MOBILE */
+@media (max-width: 768px) {
+  .payment {
+    padding: 20px;
+    flex-direction: column;
   }
 }
 </style>
