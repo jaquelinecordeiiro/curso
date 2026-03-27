db2fbe092765c11f0d6d0873c20ec493aaf37d8f
jaquelinecordeiiro <jaquelinecordeiro01@gmail.com>
2026-03-26 21:55:21 -0300

    alterando layout da payment

--- restaurante-vue2/src/components/Order.vue
+++ restaurante-vue2/src/components/Order.vue
@@ -1,20 +1,28 @@
 <template>
     <div class="order">
-        teste
-        
+        <form>
+            <label for="">Nome</label>
+            <input type="text" placeholder="Digite seu nome">
+            <p>O nome é obrigatório</p>
+        </form>
+        <button class="primary-buttom" @click="orderItens">Concluir Pedido</button>
     </div>
 
 </template>
 
 <script>
 export default {
-    name: "Order",
-};
+    methods(){
+
+    }
+  
+}
 </script>
 
 <style lang="less" scoped>
 .order {
     width: 720px;
+    margin:30px auto;
     background: white;
     border-radius: 10px;
     padding: 30px 50px;
--- restaurante-vue2/src/views/Payment.vue
+++ restaurante-vue2/src/views/Payment.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="payment">
-        <div class="payment-content">
-                <router-link to="/" class="payment--go-back" v-if="isSmallScreens()">← Voltar</router-link>
-                <Order />
+        <div class="payment--content">
+                <router-link to="/" class="payment--go-back">← Voltar</router-link>
+                <Order/>
             <Cart v-if="isDesktop()" />
 
 
@@ -16,33 +16,32 @@ import Cart from "@/components/Cart.vue";
 import Order from "@/components/Order.vue";
 
 export default {
-    mixins: [Mixins],
-    components: {
-        Cart,
-        Order,
+     components: {
+      Cart,
+      Order
     },
-
+    mixins: [Mixins]
 };
 </script>
 
 <style lang="less" scoped>
-.payment {
-  display: flex;
-  padding: 50px;
+  .payment {
+    display: flex;
+    
+    .payment--content {
+      flex-grow: 1;
+      padding: 50px;
 
-  .payment-content {
-    flex-grow: 1;
+        .payment--go-back {
+        display: inline-block;
+        margin-bottom: 20px;
+        font-weight: 600;
+        font-size: 18px;
+        text-decoration: none;
+        color: @yellow;
+      }
+    }
   }
-
-  .payment--go-back {
-    display: inline-block;
-    margin-bottom: 20px;
-    font-weight: 600;
-    font-size: 18px;
-    text-decoration: none;
-    color: @yellow;
-  }
-}
 
 /* MOBILE */
 @media (max-width: 768px) {
