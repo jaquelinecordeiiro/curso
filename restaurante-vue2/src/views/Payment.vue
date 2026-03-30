dc2129dd3d221a15a97acbaee896b9574ecdd033
jaquelinecordeiiro <jaquelinecordeiro01@gmail.com>
2026-03-30 16:50:08 -0300

    responsividade do payment

--- restaurante-vue2/src/views/Payment.vue
+++ restaurante-vue2/src/views/Payment.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="payment">
-        <div class="payment--content">
-                <router-link to="/" class="payment--go-back">← Voltar</router-link>
-                <Order/>
-            
-        </div>
-        <Cart v-if="isDesktop()" />
+  <div class="payment">
+    <div class="payment--content">
+      <router-link to="/" class="payment--go-back">← Voltar</router-link>
+      <Order />
+
     </div>
+    <Cart v-if="isDesktop()" />
+  </div>
 </template>
 
 <script>
@@ -15,40 +15,39 @@ import Cart from "@/components/Cart.vue";
 import Order from "@/components/Order.vue";
 
 export default {
-     components: {
-      Cart,
-      Order
-    },
-    mixins: [Mixins]
+  components: {
+    Cart,
+    Order
+  },
+  mixins: [Mixins]
 };
 </script>
 
 <style lang="less" scoped>
-  .payment {
-    display: flex;
-    
-    &--content {
-      flex-grow: 1;
-      padding: 50px;
+.payment {
+  display: flex;
 
-        .payment--go-back {
-        display: inline-block;
-        margin-bottom: 20px;
-        font-weight: 600;
-        font-size: 18px;
-        text-decoration: none;
-        color: @yellow;
-      }
+  &--content {
+    flex-grow: 1;
+    padding: 50px;
+
+    .payment--go-back {
+      display: inline-block;
+      margin-bottom: 20px;
+      font-weight: 600;
+      font-size: 18px;
+      text-decoration: none;
+      color: @yellow;
     }
   }
+}
 
 /* MOBILE */
 @media @tablets {
 
-    &--content{
-      padding: 50px 20px;
+  &--content {
+    padding: 50px 20px;
 
-    }
   }
-
+}
 </style>
